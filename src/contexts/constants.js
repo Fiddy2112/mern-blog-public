962e0e0840053ee99c07f02cda4c25ccfbd0deab
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-12-13 19:37:18 +0700

    deploy code

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiURL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "deloyedUrl";
+    : "https://mern-blog-api.onrender.com";
 
 export const LOCAL_STORAGE_TOKEN = "localStorageToken";
